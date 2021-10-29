c0d491c475448198dfd29329ed4e9430fec24eb6
thainguyen <thainm.uet@gmail.com>
2021-10-30 03:49:13 +0700

    Added basic update feature

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,8 @@
 const User = require('../models/user');
 const session = require('express-session');
 
+
+// This function is for testing purpose only!
 module.exports.renderLogin = (req, res) => {
     res.set('Access-Control-Allow-Origin', '*');
     res.json({ auth: true });
@@ -8,11 +10,12 @@ module.exports.renderLogin = (req, res) => {
     // res.render('users/login');
 };
 
+// This function should be authenticated by middleware function.
 module.exports.login = async function(req, res) {
     res.set('Access-Control-Allow-Origin', '*');
-    console.log("OK");
     const { username } = req.body;
-    const user = await User.findOne({ username: '19021363' });
+    // '19021363' username is for testing purpose only, please use username in request.body instead.
+    const user = await User.findOne({ username: '19021363' }); 
     res.json({
         auth: true,
         firstName: user.firstName,
@@ -22,10 +25,12 @@ module.exports.login = async function(req, res) {
     res.send();
 };
 
+// This function is for testing purpose only!
 module.exports.renderRegister = (req, res) => {
     res.render('users/register');
 };
 
+// This function is for testing purpose only!
 module.exports.register = async function(req, res) {
     const { username, password, firstName, lastName, role, email, phoneNumber } = req.body;
     const user = new User({
@@ -40,4 +45,30 @@ module.exports.register = async function(req, res) {
     await user.save();
     req.session.user_id = user._id;
     res.redirect('homepage');
+};
+
+// This function is for testing purpose only!
+module.exports.renderUpdate = (req, res) => {
+    res.render('users/update');
+};
+
+module.exports.update = async function(req, res) {
+    const { id, firstName, lastName, email, phoneNumber } = req.body;
+
+    const query = {
+        username: id
+    }
+
+    const user = await User.findOneAndUpdate(query, {
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        phoneNumber: phoneNumber
+    })
+    await user.save();
+    
+    res.json({
+        status: "OK"
+    })
+    res.send()
 };
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -7,10 +7,16 @@ const router = express.Router();
 
 router.route('/login')
     .get(users.renderLogin) // Delete this line if you only want to render html files from client side.
-    .post( users.login);
+    .post(users.login);
+    // .post(authenticate, users.login); // Uncomment this line and comment out the line above to use authenticate middleware 
+    
 
 router.route('/register')
     .get(users.renderRegister)
     .post(users.register);
+
+router.route('/update')
+    .get(users.renderUpdate)
+    .post(users.update);
 
 module.exports = router;
