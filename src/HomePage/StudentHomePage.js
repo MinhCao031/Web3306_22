3665cb3ebcdbd7ea1b296d1318bb8845ece7625d
Minh Cao <19020063@vnu.edu.vn>
2021-11-03 22:14:48 +0700

    Delete src directory

--- src/HomePage/StudentHomePage.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const StudentHomePage = ({ firstName, lastName, role }) => {
-  return <div>Teacher Homepage</div>;
-};
-
-export default StudentHomePage;
--- src/HomePage/TeacherHomePage.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-
-const TeacherHomePage = ({ firstName, lastName, role }) => {
-    return <Navbar />
-};
-
-export default TeacherHomePage;
