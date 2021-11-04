51bf5b4805569ae34231e09f23d4817e8437e8c7
Minh Cao <19020063@vnu.edu.vn>
2021-11-04 17:22:15 +0700

    Update import & fix code bug

--- client/src/HomePage/components/Navbar.js
+++ client/src/HomePage/components/Navbar.js
@@ -8,6 +8,7 @@ import { IconContext } from 'react-icons';
 import boy from '../../assets/boy_ava.png';
 import logo from '../../assets/uet.png';
 import ChangeInfoTeacher from './ChangeInfoTeacher';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function Navbar({ dataDisplay }) {
   const [sidebar, setSidebar] = useState(false);
@@ -31,6 +32,7 @@ function Navbar({ dataDisplay }) {
             <IoIcons.IoMdNotifications style={{ color: '#404E68' }} size={30} />
             <span className="profile-text">
               {`${dataDisplay.firstName} ${dataDisplay.lastName}`}
+              {/*Mai Nhật Quang*/}
             </span>
             <img alt={'Avatar'} src={boy} />
           </div>
@@ -44,18 +46,18 @@ function Navbar({ dataDisplay }) {
             : { maxWidth: contentWidth }
         }
       >
-        {/* add content like <Forum /> */
-        <img
-          src={
+        {/* add content like <Forum /> 
+        <img src={
             'https://static.bongda24h.vn/medias/standard/2021/10/22/bruno-fernandes-len-tieng-ve-tuong-lai-hlv-solskjaer.jpg'
-          } alt="test"/>
+          } alt="test"
+        />
         <span>
           I will add random shit here just to test if my sidebar and content is
           working perfectly. You don't need to read this anymore. Why do you
           still read? I said STOP!! Hey, I am not messing with you, you need to
           stop reading right fk now. So you just won't give up, will you? Okay,
           whatever, suit yourself
-        </span>}
+        </span>*/}
         <Router><Switch>
           <Route path="/teacherHomepage" exact>
           </Route>
