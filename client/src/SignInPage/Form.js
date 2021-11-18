c6bf9c60ec07141b9053a3142d5e2032c2ea4446
ilovengochuyen <19020067@vnu.edu.vn>
2021-11-18 10:52:04 +0700

    importFile

--- client/src/SignInPage/Form.js
+++ client/src/SignInPage/Form.js
@@ -24,27 +24,27 @@ const Form = () => {
   const signIn = (e) => {
     const { username, password } = user;
     if (username && password) {
-      axios
-        .post('http://localhost:5000/login', user)
-        .then((res) => {
-          if (res.data.auth === false) {
-            setMessage('Tài khoản hoặc mật khẩu không hợp lệ !');
-          } else {
-            if (res.data.role === 'Student') {
-              history.push({
-                pathname: '/studentHomepage',
-                state: res.data,
-              });
-            } else if (res.data.role === 'Teacher') {
-              history.push({
-                pathname: '/teacherHomepage',
-                state: res.data,
-              });
-            }
-          }
-        })
-        .catch((err) => console.log('error'));
-      history.push('/teacherHomepage');
+      // axios
+      //   .post('http://localhost:5000/login', user)
+      //   .then((res) => {
+      //     if (res.data.auth === false) {
+      //       setMessage('Tài khoản hoặc mật khẩu không hợp lệ !');
+      //     } else {
+      //       if (res.data.role === 'Student') {
+      //         history.push({
+      //           pathname: '/studentHomepage',
+      //           state: res.data,
+      //         });
+      //       } else if (res.data.role === 'Teacher') {
+      //         history.push({
+      //           pathname: '/teacherHomepage',
+      //           state: res.data,
+      //         });
+      //       }
+      //     }
+      //   })
+      //   .catch((err) => console.log('error'));
+      // history.push('/teacherHomepage');
     } else {
       setMessage('Vui lòng nhập đầy đủ thông tin !');
     }
