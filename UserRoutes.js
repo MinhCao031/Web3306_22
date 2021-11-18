d8e9421cab171279aa5f7361518fc5da3d5c3f16
ilovengochuyen <61539998+ilovengochuyen@users.noreply.github.com>
2021-11-18 09:28:45 +0700

    importFile

---
+++ UserRoutes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const users = require('../controllers/UserController');
+
+const middlewares = require('../middleware');
+
+const router = express.Router();
+
+router.post('/login', middlewares.authenticate, users.login);
+
+router.get('/logout', middlewares.isLoggedIn, users.logout);
+
+router.route('/:user_id/show').get(users.getInfo); // Login required
+
+router.route('/:user_id/update').post(users.update); // Login required
+
+//newT
+router.route('/:user_id/importFile').post(users.importFile) // Login required and be teacherId
+
+router.route('/:user_id/set_password').post(users.setPassword); // Login required
+
+module.exports = router;
