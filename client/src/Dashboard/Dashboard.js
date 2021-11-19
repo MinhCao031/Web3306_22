87c356beabc538e730d30c345b3111de83301cbe
Quang <mnquang123@gmail.com>
2021-11-19 18:19:04 +0700

    Basic dashboard without css

--- client/src/Dashboard/Dashboard.js
+++ client/src/Dashboard/Dashboard.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Sidebar from '../HomePage/components/Sidebar';
 import NavigationBar from '../HomePage/components/NavigationBar';
+import Footer from '../HomePage/components/Footer';
+import BarChart from './BarChart';
+import PieChart from './PieChart';
 const DashBoard = () => {
   return (
     <>
       <NavigationBar />
-      <div>Day la Dashboard</div>
+      <BarChart />
+      <PieChart />
       <Sidebar />
+      <Footer />
     </>
   );
 };
