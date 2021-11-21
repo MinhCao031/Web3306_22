f5389851c7da24055750696da37959668e202489
Quang <mnquang123@gmail.com>
2021-11-21 23:36:26 +0700

    fix sth

--- client/src/Dashboard/ClassesDropDown.js
+++ client/src/Dashboard/ClassesDropDown.js
@@ -2,9 +2,12 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 const ClassesDropDown = ({ setSelectedClassID }) => {
   const [classList, setClassList] = useState([]);
+  const username = JSON.parse(sessionStorage.getItem('user'))
+    ? JSON.parse(sessionStorage.getItem('user')).username
+    : '';
   useEffect(() => {
     axios
-      .get('http://localhost:3002/class')
+      .get(`http://localhost:5000/api/classes/${username}/show`)
       .then((res) => {
         setClassList(
           res.data.map((item) => {
--- client/src/HomePage/StudentHomePage.js
+++ client/src/HomePage/StudentHomePage.js
@@ -2,13 +2,14 @@ import React from 'react';
 import NavigationBar from './components/NavigationBar';
 import SidebarStudent from './components/SidebarStudent';
 import { Container } from './TeacherHomePage';
+
 import Footer from './components/Footer';
 function StudentHomePage() {
   return (
     <>
       <NavigationBar />
-      <Container />
       <SidebarStudent />
+      <Container />
       <Footer />
     </>
   );
