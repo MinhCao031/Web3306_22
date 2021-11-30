2352d0339d538a68638c28085a07a2b4e997379e
thanhtvt <19021367@vnu.edu.vn>
2021-11-30 15:26:57 +0700

    usage for forum btn

--- client/src/Forum/StudentForum.js
+++ client/src/Forum/StudentForum.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import SidebarStudent from '../HomePage/components/SidebarStudent';
 import NavigationBar from '../HomePage/components/NavigationBar';
+import ModalBtn from './Posts/ModalBtn';
+
 const StudentForum = () => {
-  return (
-    <>
-      <NavigationBar />
-      <div style={{ display: 'flex', justifyContent: 'center' }}>
-        <img
-          src="https://www.csp.org.uk/sites/default/files/media-image/2019-05/forum_discussion.jpg"
-          alt="Day la forum"
-        />
-      </div>
-      <SidebarStudent />
-    </>
-  );
+    return (
+        <>
+        <NavigationBar />
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <ModalBtn 
+                btnName="Đăng bài"
+                title="TẠO MỘT BÀI ĐĂNG MỚI"
+                submitBtn="Đăng bài"
+            />
+            <ModalBtn 
+                btnName=""
+                title="SỬA BÀI ĐĂNG"
+                submitBtn="Cập nhật"
+                headingText="Hỏi thông tin học bổng 100 củ"
+                contentText="Em được biết là trường đang phát tiền miễn phí, cho em hỏi thêm thông tin với ạ"
+                edit='true'
+            />
+        </div>
+        <SidebarStudent />
+        </>
+    );
 };
 
 export default StudentForum;
