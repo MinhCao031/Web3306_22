95111df47554de58100de1c9ce4f19de452c2f86
Minh Cao <19020063@vnu.edu.vn>
2021-12-02 17:31:32 +0700

    Add post & comments

--- client/src/Forum/Posts/Comment.js
+++ client/src/Forum/Posts/Comment.js
@@ -1,51 +1,85 @@
-import React, { PureComponent, useState } from 'react';
-import OnlyCmt from './onlyCmt';
-import data from './data.json';
-import './Comment.css';
+import React, { PureComponent, useState } from 'react'
+import OnlyCmt from './onlyCmt'
+import data from "./data.json"
+import "./Comment.css"
 
 const Comment = () => {
-  const [comment, setComment] = useState(data);
-  const userId = '01a';
-  const name = 'xyz';
-  const signinUrl = '/';
-  const signupUrl = '/';
-  let count = 0;
-  comment.map((i) => {
-    count += 1;
-  });
+    const [comment, setComment] = useState(Array.from(data))
+    const [yourCmt, setYourCmt] = useState({
+      "id": 0,
+      "cmtId": "",
+      "fullName": "",
+      "timeAgo": "1 phút trước",
+      "text": ""    
+    })
+    const [cmtComponent, setCmtComponent] = useState(<OnlyCmt data={comment}/>)
 
-  return (
-    <div className="pop-up-post">
-      <div className="post-content">
-        <div className="post-author-and-time">
-          <p className="post-of-sb">Nguyễn Minh Thái</p>
-          <div className="separator"></div>
-          <p className="time-taken">13 giờ trước</p>
+
+    const userId = "01a"
+    const name = "xyz"
+    const signinUrl = "/"
+    const signupUrl = "/"
+
+    let count = 0
+    comment.map(each => {count+=1})
+
+    const postCmt = () => {
+      let newCmt = comment;
+      newCmt.push(yourCmt);
+      setComment(newCmt);
+      setCmtComponent(<OnlyCmt data={comment}/>)
+      count += 1;
+      console.log(comment);
+      document.getElementById("commentDetail").value = "";
+    }
+  
+    const saveCmt = () => {
+      setYourCmt({
+        id: count+1,
+        cmtId: "",
+        fullName: name,
+        timeAgo: "1 phút trước",
+        text: document.getElementById("commentDetail").value,
+      })
+      console.log(document.getElementById("commentDetail").value)
+    }
+  
+    return (
+      <div className="pop-up-post">  
+        <div className="post-content">
+          <div className="post-author-and-time">
+            <p className="post-of-sb">Nguyễn Minh Thái</p>
+            <div className="separator"></div>
+            <p className="time-taken">13 giờ trước</p>
+          </div> 
+          <div className="post-header">
+            <p>Tiêu đề: </p>
+            <div className="separator"></div>
+            <p>Hỏi thông tin học bổng $60000 vừa giành được</p>
+          </div> 
+          <hr/>
+          <p className="post-detail">
+            Kính gửi thầy,
+            Em tên là Nguyễn Minh Thái, MSSV 19021420, 
+            Hôm qua em có biết được do quá ao chình các bạn còn lại trong quá trình học tập, làm hồ sơ và phỏng vấn nên em đã trúng tuyển suất học bổng trị giá $50000 ở Đại học Havard, cộng thêm khoản tiền hỗ trợ $10000 từ Chính phủ. Ban đầu em chỉ định nộp chơi chơi vì nghĩ Havard chưa đủ tầm với mình nên cũng không tìm hiểu kỹ, nay em mới biết thêm là giờ du học là vua của mọi nghề, vừa có tiền, vừa có quyền, vừa được xã hội trọng vọng, phụ nữ nghe xong thì lênh láng nước, đủ tạo thành mùa lũ để đi từ thiện nên em cũng đổi ý định. Vậy thầy có thể cho em hỏi thêm về thông tin về suất học bổng đó được không ạ? Em cảm ơn thầy nhiều ạ.
+          </p>
+          <hr/>
         </div>
-        <div className="post-header">
-          <p>Tiêu đề: </p>
-          <div className="separator"></div>
-          <p>Hỏi thông tin học bổng $60000 vừa giành được</p>
-        </div>
-        <hr />
-        <p className="post-detail">
-          Kính gửi thầy, Em tên là Nguyễn Minh Thái, MSSV 19021420, Hôm qua em
-          có biết được do quá ao chình các bạn còn lại trong quá trình học tập,
-          làm hồ sơ và phỏng vấn nên em đã trúng tuyển suất học bổng trị giá
-          $50000 ở Đại học Havard, cộng thêm khoản tiền hỗ trợ $10000 từ Chính
-          phủ. Ban đầu em chỉ định nộp chơi chơi vì nghĩ Havard chưa đủ tầm với
-          mình nên cũng không tìm hiểu kỹ, nay em mới biết thêm là giờ du học là
-          vua của mọi nghề, vừa có tiền, vừa có quyền, vừa được xã hội trọng
-          vọng, phụ nữ nghe xong thì lênh láng nước, đủ tạo thành mùa lũ để đi
-          từ thiện nên em cũng đổi ý định. Vậy thầy có thể cho em hỏi thêm về
-          thông tin về suất học bổng đó được không ạ? Em cảm ơn thầy nhiều ạ.
-        </p>
-        <hr />
-      </div>
-      <p> {count} Comments</p>
-      <OnlyCmt data={comment} />
-    </div>
-  );
-};
+        <div className="typeComment">
+          <input 
+            type="text" 
+            name="commentDetail" 
+            id="commentDetail" 
+            placeholder="Nhập bình luận..."
+            onChange={saveCmt}
+          />
+          <input type="submit" onClick={postCmt} value="Trả lời"/>
+        </div>        
+        <p> {count} Comments </p>
+        {/* <OnlyCmt data={comment} /> */}
+        {cmtComponent}
+      </div>  
+    )
+}
 
 export default Comment;
--- client/src/Forum/Posts/onlyCmt.js
+++ client/src/Forum/Posts/onlyCmt.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { Remarkable } from 'remarkable';
-import './Comment.css';
+import "./Comment.css";
+import count from "./Comment";
 
 //Original way to add markups that React prevents
 class Comment extends Component {
@@ -9,52 +10,99 @@ class Comment extends Component {
     console.log(props);
   }
   render = () => {
-    var md = new Remarkable();
+    // var md = new Remarkable();
     return (
       <div className="single-comment">
         <div className="comment-header">
-          <p className="comment-author">{this.props.fullName}</p>
+          <p className="comment-author">
+            {this.props.fullName}
+          </p>
           <div className="separator"></div>
-          <p className="time-ago">{this.props.timeAgo}</p>
+          <p className="time-ago">
+            {this.props.timeAgo}
+          </p>
         </div>
         {/* {md.render(this.props.children.toString())} */}
-        <div className="comment-detail">{this.props.text}</div>
+        <div className="comment-detail">
+            {this.props.text}
+        </div>
       </div>
     );
-  };
-}
+  }
+};
 
 class CommentList extends Component {
   constructor(props) {
     super(props);
   }
   render = () => {
-    var commentNodes = this.props.data.map(function (comment) {
-      return (
-        <Comment
-          fullName={comment.fullName}
-          timeAgo={comment.timeAgo}
-          key={comment.id}
-          text={comment.text}
+    var listOfComments = []
+    for (let i in this.props.data) {
+      listOfComments.push(
+        <Comment 
+          fullName={i.fullName}
+          timeAgo={i.timeAgo}
+          key={i.id}
+          text={i.text}
         />
       );
-    });
-    return <div className="commentList">{commentNodes}</div>;
-  };
-}
+    };
+    var commentNodes = Array.isArray(this.props.data)
+    ? this.props.data.map(function(cmt) {
+      return (
+        <Comment 
+          fullName={cmt.fullName}
+          timeAgo={cmt.timeAgo}
+          key={cmt.id}
+          text={cmt.text}
+        />
+      );
+    })
+    : null
+    ;
+    return (
+      <div className="commentList">
+        {commentNodes == null ? listOfComments : commentNodes}
+      </div>
+    );
+  }
+};
+
+
 
 class OnlyCmt extends Component {
   constructor(props) {
     super(props);
+
   }
+  // postCmt = () => {
+  //   let newCmt = this.props.data;
+  //   console.log(newCmt)
+  //   newCmt = newCmt.push(this.state.yourCmt)
+  //   this.props = newCmt
+  //   // count += 1
+  //   console.log(this.props);
+  // }
+
+  // saveCmt = () => {
+  //   this.state.yourCmt = {
+  //     id: count+1,
+  //     cmtId: "",
+  //     fullName: this.state.name,
+  //     timeAgo: "1 phút trước",
+  //     text: document.getElementById("commentDetail").value,
+  //   }
+  //   console.log(document.getElementById("commentDetail").value)
+  // }
   render = () => {
     return (
       <div className="only-cmt">
         <CommentList data={this.props.data} />
       </div>
     );
-  };
-}
+  }
+};
+
 
 export default OnlyCmt;
 //  <OnlyCmt data={data} />
