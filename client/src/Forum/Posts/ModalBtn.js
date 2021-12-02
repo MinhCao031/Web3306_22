a399bac78828a2773c3fe8472c4d27ac1f6c0563
thanhtvt <19021367@vnu.edu.vn>
2021-12-02 23:12:21 +0700

    create filter selection

--- client/src/Forum/Posts/ModalBtn.js
+++ client/src/Forum/Posts/ModalBtn.js
@@ -10,113 +10,116 @@ import { TextField } from '@mui/material';
 import './Modal.css';
 
 const boxStyle = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 900,
-  height: 600,
-  bgcolor: '#F4F5FA',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 900,
+    height: 600,
+    bgcolor: '#F4F5FA',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
 };
 
 function postBtn({ btnName, handleClickOpen }) {
-  return (
-    <Button variant="contained" color="primary" onClick={handleClickOpen}>
-      {btnName}
-    </Button>
-  );
+    return (
+        <Button variant="contained" color="primary" onClick={handleClickOpen}>
+        {btnName}
+        </Button>
+    );
 }
 
 function editBtn({ handleClickOpen }) {
-  return (
-    <IconButton color="default" aria-label="edit" onClick={handleClickOpen}>
-      <EditIcon />
-    </IconButton>
-  );
+    return (
+        <IconButton color="default" aria-label="edit" onClick={handleClickOpen}>
+        <EditIcon />
+        </IconButton>
+    );
 }
 
 //codeTT
 function deleteBtn({ handleClickOpen }) {
-  return (
-    <IconButton color="default" aria-label="delete" onClick={handleClickOpen}>
-      <DeleteIcon />
-    </IconButton>
-  );
+    return (
+        <IconButton color="default" aria-label="delete" onClick={handleClickOpen}>
+        <DeleteIcon />
+        </IconButton>
+    );
 }
 
 function ModalBtn({
-  btnName,
-  title,
-  submitBtn,
-  headingText = '',
-  contentText = '',
-  edit = false,
+    btnName,
+    title,
+    submitBtn,
+    headingText = '',
+    contentText = '',
+    edit = false,
 }) {
-  const [open, setOpen] = React.useState(false);
+    const [open, setOpen] = React.useState(false);
 
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
 
-  const handleClose = () => {
-    setOpen(false);
-  };
+    const handleClose = () => {
+        setOpen(false);
+    };
 
-  return (
-    <div>
-      {edit
-        ? editBtn({ handleClickOpen: handleClickOpen })
-        : postBtn({ btnName: btnName, handleClickOpen })}
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={boxStyle}>
-          {/* Move typography to center of Box */}
-          <Typography
-            variant="h4"
-            id="modal-modal-title"
-            align="center"
-            sx={{ mb: 2.5 }}
-          >
-            {title}
-          </Typography>
+    return (
+        <div>
+        {edit
+            ? editBtn({ handleClickOpen: handleClickOpen })
+            : postBtn({ btnName: btnName, handleClickOpen })}
+        <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+        >
+            <Box sx={boxStyle}>
+            {/* Move typography to center of Box */}
+            <Typography
+                variant="h4"
+                id="modal-modal-title"
+                align="center"
+                sx={{ mb: 2.5 }}
+            >
+                {title}
+            </Typography>
 
-          <TextField
-            id="filled-basic"
-            label="Tiêu đề"
-            variant="filled"
-            defaultValue={headingText}
-            fullWidth
-            sx={{ mb: 2.5 }}
-          />
+            <TextField
+                required
+                id="filled-basic"
+                label="Tiêu đề"
+                variant="filled"
+                defaultValue={headingText}
+                fullWidth
+                sx={{ mb: 2.5 }}
+                inputProps={{ maxLength: 80 }}
+            />
 
-          <TextField
-            id="filled-basic"
-            label="Nội dung"
-            variant="filled"
-            defaultValue={contentText}
-            rows={12}
-            fullWidth
-            multiline
-          />
-          <div className="btn-forum">
-            <Button variant="contained" color="error" onClick={handleClose}>
-              Hủy
-            </Button>
+            <TextField
+                required
+                id="filled-basic"
+                label="Nội dung"
+                variant="filled"
+                defaultValue={contentText}
+                rows={12}
+                fullWidth
+                multiline
+            />
+            <div className="btn-forum">
+                <Button variant="contained" color="error" onClick={handleClose}>
+                Hủy
+                </Button>
 
-            <Button variant="contained" color="primary" sx={{ ml: 4 }}>
-              {submitBtn}
-            </Button>
-          </div>
-        </Box>
-      </Modal>
-    </div>
-  );
+                <Button variant="contained" color="primary" sx={{ ml: 4 }}>
+                {submitBtn}
+                </Button>
+            </div>
+            </Box>
+        </Modal>
+        </div>
+    );
 }
 export default ModalBtn;
--- client/src/Lists/Filters/FilterButton.js
+++ client/src/Lists/Filters/FilterButton.js
@@ -1,91 +1,134 @@
 import React, { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import './Filter.css';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
 import axios from 'axios';
-const FilterButton = ({ type, setData }) => {
-  const [badFlip, setBadFlip] = useState(false);
-  const [goodFlip, setGoodFlip] = useState(false);
-  const classId = JSON.parse(sessionStorage.getItem('TableInfo'))
-    ? JSON.parse(sessionStorage.getItem('TableInfo')).classId
-    : '';
-  const username = JSON.parse(sessionStorage.getItem('user'))
-    ? JSON.parse(sessionStorage.getItem('user')).username
-    : '';
-  function isBad(rec) {
-    if (rec.gpa < 2.0 || rec.drl < 60) {
-      return true;
-    }
-    return false;
-  }
-  const handleBadClick = (e) => {
-    axios
-      .post(`http://localhost:5000/api/classes/students`, null, {
-        params: {
-          class_id: classId,
-          role: 'Teacher',
-          user_id: username,
-        },
-      })
-      .then((res) => {
-        setData(res.data.filter(isBad));
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-    e.preventDefault();
-  };
+import './Filter.css';
+import PopOverHover from './PopOverHover';
 
-  function isGood(rec) {
-    if (rec.gpa >= 3.2 && rec.drl >= 80) {
-      return true;
-    }
-    return false;
-  }
-  const handleGoodClick = (e) => {
-    axios
-      .post(`http://localhost:5000/api/classes/students`, null, {
-        params: {
-          class_id: classId,
-          role: 'Teacher',
-          user_id: username,
-        },
-      })
-      .then((res) => {
-        setData(res.data.filter(isGood));
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-    e.preventDefault();
-  };
+const FilterButton = ({ setData }) => {
+    const [filterType, setFilterType] = useState('');
 
-  if (type === 'bad') {
+    const classId = JSON.parse(sessionStorage.getItem('TableInfo'))
+        ? JSON.parse(sessionStorage.getItem('TableInfo')).classId
+        : '';
+    const username = JSON.parse(sessionStorage.getItem('user'))
+        ? JSON.parse(sessionStorage.getItem('user')).username
+        : '';
+
+    const handleFilterType = (e) => {
+        setFilterType(e.target.value);
+    }
+
+    function isBad(rec) {
+        if (rec.gpa < 2.0 || rec.drl < 60) {
+            return true;
+        }
+        return false;
+    }
+
+    const handleBadClick = (e) => {
+        axios
+        .post(`http://localhost:5000/api/classes/students`, null, {
+            params: {
+            class_id: classId,
+            role: 'Teacher',
+            user_id: username,
+            },
+        })
+        .then((res) => {
+            setData(res.data.filter(isBad));
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+        e.preventDefault();
+    };
+
+    function isGood(rec) {
+        if (rec.gpa >= 3.2 && rec.drl >= 80) {
+            return true;
+        }
+        return false;
+    }
+
+    const handleGoodClick = (e) => {
+        axios
+        .post(`http://localhost:5000/api/classes/students`, null, {
+            params: {
+            class_id: classId,
+            role: 'Teacher',
+            user_id: username,
+            },
+        })
+        .then((res) => {
+            setData(res.data.filter(isGood));
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+        e.preventDefault();
+    };
+
+    const handleFeeClick = (e) => {
+        console.log('Thiếu học phí');
+    };
+
+    const handleCreditsClick = (e) => {
+        console.log('Thiếu tín chỉ');
+    };
+
+    const handleExpelledClick = (e) => {
+        console.log('Nguy cơ nghỉ học');
+    }
+
     return (
-      <Button
-        className="ListButton filter"
-        variant="outline-secondary"
-        onClick={handleBadClick}
-        onMouseEnter={() => setBadFlip(true)}
-        onMouseLeave={() => setBadFlip(false)}
-      >
-        {badFlip && <span>GPA dưới 2.0 || ĐRL dưới 60</span>}
-        {!badFlip && <span>Lọc: Thuộc diện nhắc nhở</span>}
-      </Button>
-    );
-  } else if (type === 'good') {
-    return (
-      <Button
-        className="ListButton filter"
-        variant="outline-secondary"
-        onClick={handleGoodClick}
-        onMouseEnter={() => setGoodFlip(true)}
-        onMouseLeave={() => setGoodFlip(false)}
-      >
-        {goodFlip && <span>GPA trên 3.2 & ĐRL trên 80</span>}
-        {!goodFlip && <span>Lọc: Thuộc diện khen thưởng</span>}
-      </Button>
-    );
-  }
+        <>
+            <InputLabel id="select-filter-label" sx={{marginLeft: 1.9}}> Tiêu chí lọc </InputLabel>
+            <Select
+                labelId="select-filter-label"
+                id="select-filter"
+                value={filterType}
+                label='Tiêu chí lọc'
+                onChange={handleFilterType}
+            >
+                <MenuItem 
+                    value={'good'}
+                    onClick={handleGoodClick}
+                >
+                    <PopOverHover tag={'good'} />
+                </MenuItem>
+
+                <MenuItem 
+                    value={'bad'}
+                    onClick={handleBadClick}
+                >
+                    <PopOverHover tag={'bad'} />
+                </MenuItem>
+                
+                <MenuItem
+                    value={'fee'}
+                    onClick={handleFeeClick}
+                >
+                    {'Thiếu học phí'}
+                </MenuItem>
+
+                <MenuItem
+                    value={'credits'}
+                    onClick={handleCreditsClick}
+                >
+                    {'Thiếu tín chỉ'}
+                </MenuItem>
+
+                <MenuItem
+                    value={'expelled'}
+                    onClick={handleExpelledClick}
+                >
+                    <PopOverHover tag={'expelled'} />
+                </MenuItem>
+            </Select>
+        </>
+    )
 };
 
 export default FilterButton;
