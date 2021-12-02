95345f0d1556850421f2262a49c8d3055b668904
Minh Cao <19020063@vnu.edu.vn>
2021-12-02 10:19:47 +0700

    Redo Comment (no input yet)

--- client/src/Forum/Posts/Comment.js
+++ client/src/Forum/Posts/Comment.js
@@ -1,9 +1,7 @@
 import React, { PureComponent, useState } from 'react'
+import OnlyCmt from './onlyCmt'
 import data from "./data.json"
 import './editSrc'
-import { CommentSection } from './editSrc'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircle } from '@fortawesome/free-solid-svg-icons';
 import 'react-comments-section/dist/index.css'
 import "./Comment.css"
 
@@ -14,33 +12,31 @@ const Comment = () => {
     const signinUrl = "/"
     const signupUrl = "/"
     let count = 0
-    comment.map(i => {count+=1; i.replies && i.replies.map(i=> count+=1)} )
+    comment.map(i => {count+=1;} )
 
     return (
       <div className="pop-up-post">  
-        <div className="Author">
-          <div className="cmt-header">
-            <p className="cmtOfSb">Nguyễn Minh Thái</p>
+        <div className="post-content">
+          <div className="post-author-and-time">
+            <p className="post-of-sb">Nguyễn Minh Thái</p>
             <div className="separator"></div>
             <p className="time-taken">13 giờ trước</p>
           </div> 
+          <div className="post-header">
+            <p>Tiêu đề: </p>
+            <div className="separator"></div>
+            <p>Hỏi thông tin học bổng $60000 vừa giành được</p>
+          </div> 
           <hr/>
-            <p>
-                Kính gửi thầy,
-                Em tên là Nguyễn Minh Thái, MSSV 19021420
-                Hôm qua em có biết được do quá ao chình các bạn còn lại trong quá trình học tập, làm hồ sơ và phỏng vấn nên em đã trúng tuyển suất học bổng trị giá $50000 ở Đại học Havard, cộng thêm khoản tiền hỗ trợ $10000 từ Chính phủ. Ban đầu em chỉ định nộp chơi chơi vì nghĩ Havard chưa đủ tầm với mình nên cũng không tìm hiểu kỹ, nay em mới biết thêm là giờ du học là vua của mọi nghề, vừa có tiền, vừa có quyền, vừa được xã hội trọng vọng, phụ nữ nghe xong thì lênh láng nước, đủ tạo thành mùa lũ để đi từ thiện nên em cũng đổi ý định. Vậy thầy có thể cho em hỏi thêm về thông tin về suất học bổng đó được không ạ? Em cảm ơn thầy nhiều ạ.
-            </p>
-            <hr/>
+          <p className="post-detail">
+            Kính gửi thầy,
+            Em tên là Nguyễn Minh Thái, MSSV 19021420, 
+            Hôm qua em có biết được do quá ao chình các bạn còn lại trong quá trình học tập, làm hồ sơ và phỏng vấn nên em đã trúng tuyển suất học bổng trị giá $50000 ở Đại học Havard, cộng thêm khoản tiền hỗ trợ $10000 từ Chính phủ. Ban đầu em chỉ định nộp chơi chơi vì nghĩ Havard chưa đủ tầm với mình nên cũng không tìm hiểu kỹ, nay em mới biết thêm là giờ du học là vua của mọi nghề, vừa có tiền, vừa có quyền, vừa được xã hội trọng vọng, phụ nữ nghe xong thì lênh láng nước, đủ tạo thành mùa lũ để đi từ thiện nên em cũng đổi ý định. Vậy thầy có thể cho em hỏi thêm về thông tin về suất học bổng đó được không ạ? Em cảm ơn thầy nhiều ạ.
+          </p>
+          <hr/>
         </div>
-        <div className="comment-section">
-            <div className="header">{count} Comments</div>
-            <CommentSection currentUser={userId && { userId: userId, name: name }} 
-                commentsArray={comment}
-                setComment={setComment} 
-                signinUrl={signinUrl} 
-                signupUrl={signupUrl}
-            />
-        </div>
+        <p> {count} Comments</p>
+        <OnlyCmt data={comment} />
       </div>  
     )
 }
---
+++ client/src/Forum/Posts/onlyCmt.js
@@ -0,0 +1,73 @@
+import React, { Component } from "react";
+import { Remarkable } from 'remarkable';
+import "./Comment.css";
+
+//Original way to add markups that React prevents
+class Comment extends Component {
+  constructor(props) {
+    super(props);
+    console.log(props);
+  }
+  render = () => {
+    var md = new Remarkable();
+    return (
+      <div className="single-comment">
+        <div className="comment-header">
+          <p className="comment-author">
+            {this.props.fullName}
+          </p>
+          <div className="separator"></div>
+          <p className="time-ago">
+            {this.props.timeAgo}
+          </p>
+        </div>
+        {/* {md.render(this.props.children.toString())} */}
+        <div className="comment-detail">
+            {this.props.text}
+        </div>
+      </div>
+    );
+  }
+};
+
+class CommentList extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render = () => {
+    var commentNodes = this.props.data.map(function(comment) {
+      return (
+        <Comment 
+          fullName={comment.fullName}
+          timeAgo={comment.timeAgo}
+          key={comment.id}
+          text={comment.text}
+        />
+      );
+    });
+    return (
+      <div className="commentList">
+        {commentNodes}
+      </div>
+    );
+  }
+};
+
+
+
+class OnlyCmt extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render = () => {
+    return (
+      <div className="only-cmt">
+        <CommentList data={this.props.data} />
+      </div>
+    );
+  }
+};
+
+
+export default OnlyCmt;
+//  <OnlyCmt data={data} />
